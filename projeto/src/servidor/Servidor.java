18fd409ecb21aaaedbffbea16e15bdc6379acfe1
KIKO <fjmo_1997@hotmail.com>
2018-11-27 22:10:03 +0000

    um pouco de buffers

--- projeto/src/servidor/Servidor.java
+++ projeto/src/servidor/Servidor.java
@@ -6,54 +6,55 @@
 package servidor;
 
 import java.io.*;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  *
  * @author KIKO
  */
 public class Servidor extends Thread{
-    public final int PORT_NUMBER = 4100;
+    private AtomicBoolean running = new AtomicBoolean(true);
+    
+    public final int PORT = 4100;
     private ServerSocket serverSocket;
     private Socket clienteSocket;
      
     public Servidor() throws IOException {
-        this.serverSocket = new ServerSocket(this.PORT_NUMBER);
+        this.serverSocket = new ServerSocket(this.PORT);
     }
-     
+    
+    // Interrompe o Servidor
+    public void stopServidor(){
+        this.running.set(false);
+        this.interrupt();
+        System.out.println("[Servidor] Servidor terminado !!!");
+    }
+    
     public void run() {
-        System.out.println("BATATAS");
-        while(true){
-            try{this.clienteSocket = this.serverSocket.accept();}
-            catch(IOException e){
-                System.out.println("Erro a aceitar ligação do cliente\n" + e);
+        System.out.println("[Servidor] Iniciando o Servidor");
+        while(this.running.get()){
+            System.out.println("[Servidor] Servidor à escuta na porta " + 
+                                this.serverSocket.getLocalSocketAddress());
+            try{
+                // fica a escuta por pedidos
+                this.clienteSocket = this.serverSocket.accept();
             }
-            System.out.println("New client connected from " + 
-                                clienteSocket.getInetAddress().getHostAddress());
-            // cria uma nova thread para a conexao
-            new ServidorThread(clienteSocket).run();
+            catch(IOException e){
+                System.out.println("[Servidor] Erro a aceitar ligação do cliente");
+                System.out.println(e);
+            }
+            try{
+                // cria uma nova thread para a conexao
+                new ServidorThread(clienteSocket).run();
+            }
+            catch(IOException e){
+                System.out.println("[Servidor] Erro a criar Thread para o cliente");
+                System.out.println(e);
+            }
             
         }
-
-//        InputStream in = null;
-//        try {
-//            in = serverSocket.getInputStream();
-//            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-//            String request;
-//            request = br.readLine();
-//            System.out.println("Message received:" + request);
-//        } catch (IOException e) {
-//            System.out.println("Unable to get streams from client");
-//        } finally {
-//            try {
-//                in.close();
-//                serverSocket.close();
-//                System.out.println("Socket closed");
-//            } catch (Exception ex) {
-//                ex.printStackTrace();
-//            }
-//        }
-    }    
+    }
+    
 }
--- projeto/src/servidor/ServidorThread.java
+++ projeto/src/servidor/ServidorThread.java
@@ -6,22 +6,69 @@
 package servidor;
 
 import java.io.*;
-import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
 /**
  *
  * @author KIKO
  */
 public class ServidorThread extends Thread {
-    Socket clienteSocket;
+    private final Socket clienteSocket;
+    private final InputStream fromClient;
+    private final OutputStream toClient;
+    private byte[] mensagem;
     
-    public ServidorThread(Socket cliente){
+    public ServidorThread(Socket cliente) throws IOException {
         this.clienteSocket = cliente;
+        this.fromClient = cliente.getInputStream();
+        this.toClient = cliente.getOutputStream();
+        this.mensagem = new byte[1024];
     }
     
     public void run(){
-        System.out.println("batatas delivered");
+        System.out.println("  [ServidorThread] Novo cliente apartir de " + 
+                            clienteSocket.getInetAddress().getHostAddress());
+        
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(fromClient));
+            String request;
+            while( null!=(request=br.readLine()) ){
+                System.out.println("Message received:" + request);
+            }
+//            while( fromClient.read(mensagem, 0, 1024) != -1 ) {
+////                String msg = new String(packetRecebido.getData(), packetRecebido.getOffset(), packetRecebido.getLength());
+//                System.out.println("  [ServidorThread] Mensagem: ");
+//                System.out.println(mensagem);
+//                System.out.println("  [ServidorThread] End of message");
+////                toServer.write(mensagem, 0, 1024);
+////                toServer.flush();
+////                System.out.println("   [ClientTCPReceiver] flushed");
+//            }
+        }
+        catch (IOException e) { 
+            System.out.println ("  [ServidorThread] ERRO: Falha de leitura!!!"); 
+            System.out.println(e);
+        }
+        
+        System.out.println("  [ServidorThread] Cliente fechou ligação");
     }
+    
+//        try {
+//            in = serverSocket.getInputStream();
+//            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+//            String request;
+//            request = br.readLine();
+//            System.out.println("Message received:" + request);
+//        } catch (IOException e) {
+//            System.out.println("Unable to get streams from client");
+//        } finally {
+//            try {
+//                in.close();
+//                serverSocket.close();
+//                System.out.println("Socket closed");
+//            } catch (Exception ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+    
     
 }
