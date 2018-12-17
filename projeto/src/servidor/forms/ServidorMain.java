e159f9fe032953b29567475772339e7d8fe3b73f
KIKO <fjmo_1997@hotmail.com>
2018-12-17 11:43:44 +0000

    move forms to correct folder

--- projeto/src/servidor/forms/ServidorMain.java
+++ projeto/src/servidor/forms/ServidorMain.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servidor;
+package servidor.forms;
 
 import java.io.IOException;
+import servidor.Servidor;
 
 /**
  *
