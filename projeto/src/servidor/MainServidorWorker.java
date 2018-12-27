303555fc30fda923a4f214ae72263cea2a49eb75
KIKO <fjmo_1997@hotmail.com>
2018-12-27 19:11:12 +0000

    rip tcp

--- projeto/src/servidor/MainServidorWorker.java
+++ projeto/src/servidor/MainServidorWorker.java
@@ -73,31 +73,6 @@ public class MainServidorWorker extends Thread {
             System.out.println(e);
         }
         System.out.println("[Worker] EXCEPTION - Terminando conexao!!!");
-        // zona com coisas para a LIGACAO TCP
-//        System.out.println("  [ServidorThread] Novo cliente apartir de " + 
-//                            clienteSocket.getInetAddress().getHostAddress());
-//        try{
-//            BufferedReader br = new BufferedReader(new InputStreamReader(fromClient));
-//            String request;
-//            while( null!=(request=br.readLine()) ){
-//                System.out.println("Message received:" + request);
-//            }
-//            byte[] mensagem = new byte[1024];
-//            while( fromClient.read(mensagem, 0, 1024) != -1 ) {
-////                String msg = new String(packetRecebido.getData(), packetRecebido.getOffset(), packetRecebido.getLength());
-//                System.out.println("  [MainServidorWorker] Mensagem: ");
-//                System.out.println(mensagem);
-//                System.out.println("  [MainServidorWorker] End of message");
-////                toServer.write(mensagem, 0, 1024);
-////                toServer.flush();
-////                System.out.println("   [ClientTCPReceiver] flushed");
-//            }
-//        }
-//        catch (IOException e) { 
-//            System.out.println ("  [ServidorThread] ERRO: Falha de leitura!!!"); 
-//            System.out.println(e);
-//        }
-//        System.out.println("  [ServidorThread] Cliente fechou ligação");
     }
     
 }
