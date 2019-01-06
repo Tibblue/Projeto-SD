dfffe4748edc64b514ff6cf40caa29714dc06470
Gonçalo Camaz <gcamaz@sapo.pt>
2019-01-06 13:51:32 +0000

    Update ServerComparator.java

--- projeto/src/servidor/ServerComparator.java
+++ projeto/src/servidor/ServerComparator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servidor;
 
 import java.util.Comparator;
@@ -17,8 +12,8 @@ public class ServerComparator
     
     @Override
     public int compare(Server jc1, Server jc2) {             
-      return (jc2.getLastBid() < jc1.getLastBid() ? -1 :                     
-              (jc2.getLastBid() == jc1.getLastBid() ? 0 : 1));           
+      return (jc2.getLastBid() < jc1.getLastBid() ? 1 :                     
+              (jc2.getLastBid() == jc1.getLastBid() ? 0 : -1));           
     }     
   };  
 }
